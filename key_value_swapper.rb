17b900721422b431dde1891792d69d0db45b83a0
KamilahJ4e <kamilahjae@gmail.com>
2014-10-03 13:24:21 -0700

    updated comments for clearer notes.

--- key_value_swapper.rb
+++ key_value_swapper.rb
@@ -1,20 +1,23 @@
 # Make a method that takes a hash as an argument and swaps each key with it's value.
+# => {"Wabi" => :name, "Cat" => :species}
 
-
-    a = {name: "Wabi", species: "Cat"}
+a = {name: "Wabi", species: "Cat"}
     # [[],[]] iterating with a hash gives you an array of arrays
 
 
-  def self.swap(hash)
-    new_hash = {}
-    hash.each do |k, v| #k stands for name: v stands for "Wabi"
-      new_hash[v] = k
-    end
-    new_hash
+def self.swap(hash)
+  new_hash = {}
+  hash.each do |k, v| #k stands for name: v stands for "Wabi"
+    new_hash[v] = k
   end
+  new_hash
+end
 
 puts swap(a).inspect
 
+
+## Other ways to solve the problem
+# 1:
 def swap(hash)
   v = hash.values
   k = hash.keys
@@ -24,6 +27,7 @@ def swap(hash)
 
 end
 
+# 2:
 # iterate over the keys and manually assign keys and values to new hash
 def swap(hash)
   new_hash = {}
@@ -33,14 +37,5 @@ def swap(hash)
   end
   new_hash
 end
-
-    #new_hash = {}
-    #@a.keys.each {|new_hash,(k,v)| new_hash[k.to_sym] = v; new_hash}
-
-
-    # keys = @a.keys
-    # keys.each do |key|
-    # new_hash << key = a.values
-
 
 # => {"Wabi" => :name, "Cat" => :species}
